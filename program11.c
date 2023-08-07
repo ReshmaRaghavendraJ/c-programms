987547b72874aa5a8a2b7b8b5ed9628b7fe122c4
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-08-07 15:58:30 +0530

    check valid number or not

---
+++ program11.c
@@ -0,0 +1,11 @@
+int main()
+{
+    int num;
+    printf("Enter number:");
+    scanf("%d", &num);
+    if (num == 2)
+        printf("Valid Number");
+    else
+        printf("Invalid Number");
+    return 0;
+}
