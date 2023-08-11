b96b9ec0395fe81e672656a443b86b993a8e9f7c
Reshma N <ReshmaRaghavendraj@gmail.com>
2023-08-11 17:48:57 +0530

    count number of negative numbers

---
+++ program51.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int num[5], c = 0, i;
+    printf("Enter number:\n");
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%d", &num[i]);
+        if (num[i] < 0)
+            c++;
+    }
+    printf("Total count of negative number is:%d\n", c);
+    return 0;
+}
